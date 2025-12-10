acf2437402339a6830e2404376a8da32db329cf8
Omar <omar-aljalam@outlook.com>
2025-12-10 10:13:45 +0200

    Connect the WS and accept & recieve messages

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,4 +1,11 @@
 from channels.generic.websocket import WebsocketConsumer
+import json
 
 class ChatConsumer(WebsocketConsumer):
-    pass
+    def connect(self):
+        self.accept()
+
+    def receive(self, text_data):
+        message = json.loads(text_data).get("message", "")
+        print(f"Received message: {message}")
+        
