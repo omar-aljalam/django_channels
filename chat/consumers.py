79a838a64c72c2545ee582d02261f7ed6c57ea0d
Omar <omar-aljalam@outlook.com>
2025-12-11 16:15:22 +0200

    Layers and Grouped Layers

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,4 +1,7 @@
 from channels.generic.websocket import WebsocketConsumer
+
+from asgiref.sync import async_to_sync
+
 import json
 
 class ChatConsumer(WebsocketConsumer):
@@ -9,6 +12,11 @@ class ChatConsumer(WebsocketConsumer):
             "message": "WebSocket connection established.",
             "status": "connected"
         }))
+
+        async_to_sync(self.channel_layer.group_add)(
+            "chat_group",
+            self.channel_name
+        )
 
     def receive(self, text_data):
         message = json.loads(text_data).get("message", "")
