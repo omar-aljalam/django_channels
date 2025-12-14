0b20deb961bf0cf95ddb89c91755f213c937677c
Omar <omar-aljalam@outlook.com>
2025-12-15 00:43:17 +0200

    registeration

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.views.generic import View
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
 
 from channels.layers import get_channel_layer
 
@@ -26,6 +28,34 @@ class LoginView(View):
 class RegisterView(View):
     def get(self, request):
         return render(request, "chat/register.html")
+
+    def post(self, request):
+        message = {}
+        data = request.POST
+        first_name = data.get("first_name")
+        last_name = data.get("last_name")
+        username = data.get("username")
+        email = data.get("email")
+        password = data.get("password")
+
+        if User.objects.filter(username=username).exists():
+            message = {"error": "Username already exists"}
+            return render(request, "chat/register.html", message)
+        
+        if User.objects.filter(email=email).exists():
+            message = {"error": "Email already exists"}
+            return render(request, "chat/register.html", message)
+        
+        user = User.objects.create_user(username=username, email=email,  first_name=first_name, last_name=last_name)
+        user.set_password(password)
+        user.save()
+        user = authenticate(request=request, username=username,password=password)
+        if user is not None:
+            login(request=request, user=user)
+            return redirect("home")
+        else:
+            message = {"error": "Registration failed. Please try again."}
+            return render(request, "chat/register.html", message)
 
 class LogoutView(View):
     def get(self, request):
